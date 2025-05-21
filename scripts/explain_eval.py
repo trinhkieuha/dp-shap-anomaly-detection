55eac0de611e42c28e6e723ced5feade8743e34e
trinhkieuha <hktrinh2210@gmail.com>
2025-05-22 00:56:27 +0200

    Commit explain and evaluation

--- scripts/explain_eval.py
+++ scripts/explain_eval.py
@@ -5,7 +5,7 @@ sys.dont_write_bytecode = True
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 # Import relevant packages
-from src.explainability import ShapKernelExplainer
+from src.explainability import ShapKernelExplainer, ExplainabilityEvaluator
 
 # Import necessary libraries
 from datetime import datetime
@@ -20,31 +20,27 @@ def main():
     parser.add_argument("--model_type", type=str, default="baseline", help="Type of the model to explain.")
     parser.add_argument("--continue_run", type=bool, default=False, help="Continue run if already started.")
     parser.add_argument("--background_method", type=str, default="fixed", help="Background method for SHAP explanations.")
-    parser.add_argument("--background_size", type=int, default=100, help="Background size for SHAP explanations.")
-    parser.add_argument("--nsamples", type=int, default=500, help="Number of samples for SHAP explanations.")
 
     # --- Parse arguments ---
     args = parser.parse_args()
     metric = args.metric
     model_type = args.model_type
     continue_run = args.continue_run
-    background_size = args.background_size
-    nsamples = args.nsamples
     background_method = args.background_method
 
     # --- Start logging this run ---
-    log_path = "logs/explain_log.txt"
+    log_path = "logs/explain_eval_log.txt"
     os.makedirs("logs", exist_ok=True)  # Ensure log directory exists
     start_time = datetime.now()
     with open(log_path, "a") as log_file:
-        info_txt = f"\n\n=== Run started at: {start_time} ===\nArguments: metric={metric}, model_type={model_type}, continue_run={continue_run}, background_size={background_size}, nsamples={nsamples}, background_method={background_method}\n"
+        info_txt = f"\n\n=== Run started at: {start_time} ===\nArguments: metric={metric}, model_type={model_type}, continue_run={continue_run}, background_method={background_method}\n"
         log_file.write(info_txt)
     print(info_txt)
 
     # --- Run SHAP explanations ---
     try:
-        shap_init = ShapKernelExplainer(model_type=model_type, metric=metric, continue_run=continue_run)
-        shap_init.explain_all_versions(background_size=background_size, nsamples=nsamples, background_method=background_method)
+        eval_init = ExplainabilityEvaluator(model_type=model_type, metric=metric, background_method=background_method, continue_run=continue_run)
+        eval_init.evaluate_all_versions()
     except Exception as e:
         # --- Log failure with timestamp and error ---
         end_time = datetime.now()
---
+++ scripts/explain_eval.sh
@@ -0,0 +1,101 @@
+#!/bin/bash
+python3.11 "scripts/explain_eval.py" --metric AUC
+python3.11 "scripts/explain_eval.py" --metric AUC --continue_run True
+python3.11 "scripts/explain_eval.py" --metric AUC --continue_run True
+python3.11 "scripts/explain_eval.py" --metric AUC --continue_run True
+python3.11 "scripts/explain_eval.py" --metric AUC --continue_run True
+python3.11 "scripts/explain_eval.py" --metric AUC --continue_run True
+python3.11 "scripts/explain_eval.py" --metric AUC --continue_run True
+python3.11 "scripts/explain_eval.py" --metric AUC --continue_run True
+python3.11 "scripts/explain_eval.py" --metric AUC --continue_run True
+python3.11 "scripts/explain_eval.py" --metric AUC --continue_run True
+python3.11 "scripts/explain_eval.py" --metric AUC --continue_run True
+python3.11 "scripts/explain_eval.py" --metric AUC --continue_run True
+python3.11 "scripts/explain_eval.py" --metric AUC --continue_run True
+python3.11 "scripts/explain_eval.py" --metric AUC --continue_run True
+python3.11 "scripts/explain_eval.py" --metric AUC --continue_run True
+python3.11 "scripts/explain_eval.py" --metric AUC --continue_run True
+python3.11 "scripts/explain_eval.py" --metric AUC --continue_run True
+python3.11 "scripts/explain_eval.py" --metric AUC --continue_run True
+python3.11 "scripts/explain_eval.py" --metric AUC --continue_run True
+python3.11 "scripts/explain_eval.py" --metric AUC --continue_run True
+python3.11 "scripts/explain_eval.py" --metric AUC --continue_run True
+python3.11 "scripts/explain_eval.py" --metric AUC --continue_run True
+python3.11 "scripts/explain_eval.py" --metric AUC --continue_run True
+python3.11 "scripts/explain_eval.py" --metric AUC --continue_run True
+python3.11 "scripts/explain_eval.py" --metric AUC --continue_run True
+python3.11 "scripts/explain_eval.py" --metric AUC --model_type dpsgd
+python3.11 "scripts/explain_eval.py" --metric AUC --continue_run True --model_type dpsgd
+python3.11 "scripts/explain_eval.py" --metric AUC --continue_run True --model_type dpsgd
+python3.11 "scripts/explain_eval.py" --metric AUC --continue_run True --model_type dpsgd
+python3.11 "scripts/explain_eval.py" --metric AUC --continue_run True --model_type dpsgd
+python3.11 "scripts/explain_eval.py" --metric AUC --continue_run True --model_type dpsgd
+python3.11 "scripts/explain_eval.py" --metric AUC --continue_run True --model_type dpsgd
+python3.11 "scripts/explain_eval.py" --metric AUC --continue_run True --model_type dpsgd
+python3.11 "scripts/explain_eval.py" --metric AUC --continue_run True --model_type dpsgd
+python3.11 "scripts/explain_eval.py" --metric AUC --continue_run True --model_type dpsgd
+python3.11 "scripts/explain_eval.py" --metric AUC --continue_run True --model_type dpsgd
+python3.11 "scripts/explain_eval.py" --metric AUC --continue_run True --model_type dpsgd
+python3.11 "scripts/explain_eval.py" --metric AUC --continue_run True --model_type dpsgd
+python3.11 "scripts/explain_eval.py" --metric AUC --continue_run True --model_type dpsgd
+python3.11 "scripts/explain_eval.py" --metric AUC --continue_run True --model_type dpsgd
+python3.11 "scripts/explain_eval.py" --metric AUC --continue_run True --model_type dpsgd
+python3.11 "scripts/explain_eval.py" --metric AUC --continue_run True --model_type dpsgd
+python3.11 "scripts/explain_eval.py" --metric AUC --continue_run True --model_type dpsgd
+python3.11 "scripts/explain_eval.py" --metric AUC --continue_run True --model_type dpsgd
+python3.11 "scripts/explain_eval.py" --metric AUC --continue_run True --model_type dpsgd
+python3.11 "scripts/explain_eval.py" --metric AUC --continue_run True --model_type dpsgd
+python3.11 "scripts/explain_eval.py" --metric AUC --continue_run True --model_type dpsgd
+python3.11 "scripts/explain_eval.py" --metric AUC --continue_run True --model_type dpsgd
+python3.11 "scripts/explain_eval.py" --metric AUC --continue_run True --model_type dpsgd
+python3.11 "scripts/explain_eval.py" --metric AUC --continue_run True --model_type dpsgd
+python3.11 "scripts/explain_eval.py" --metric AUC --continue_run True --model_type dpsgd
+python3.11 "scripts/explain_eval.py" --metric AUC --continue_run True --model_type dpsgd
+python3.11 "scripts/explain_eval.py" --metric AUC --continue_run True --model_type dpsgd
+python3.11 "scripts/explain_eval.py" --metric AUC --continue_run True --model_type dpsgd
+python3.11 "scripts/explain_eval.py" --metric AUC --continue_run True --model_type dpsgd
+python3.11 "scripts/explain_eval.py" --metric AUC --continue_run True --model_type dpsgd
+python3.11 "scripts/explain_eval.py" --metric AUC --continue_run True --model_type dpsgd
+python3.11 "scripts/explain_eval.py" --metric AUC --continue_run True --model_type dpsgd
+python3.11 "scripts/explain_eval.py" --metric AUC --continue_run True --model_type dpsgd
+python3.11 "scripts/explain_eval.py" --metric AUC --continue_run True --model_type dpsgd
+python3.11 "scripts/explain_eval.py" --metric AUC --continue_run True --model_type dpsgd
+python3.11 "scripts/explain_eval.py" --metric AUC --continue_run True --model_type dpsgd
+python3.11 "scripts/explain_eval.py" --metric AUC --continue_run True --model_type dpsgd
+python3.11 "scripts/explain_eval.py" --metric AUC --continue_run True --model_type dpsgd
+python3.11 "scripts/explain_eval.py" --metric AUC --continue_run True --model_type dpsgd
+python3.11 "scripts/explain_eval.py" --metric AUC --continue_run True --model_type dpsgd
+python3.11 "scripts/explain_eval.py" --metric AUC --continue_run True --model_type dpsgd
+python3.11 "scripts/explain_eval.py" --metric AUC --continue_run True --model_type dpsgd
+python3.11 "scripts/explain_eval.py" --metric AUC --continue_run True --model_type dpsgd
+python3.11 "scripts/explain_eval.py" --metric AUC --continue_run True --model_type dpsgd
+python3.11 "scripts/explain_eval.py" --metric AUC --continue_run True --model_type dpsgd
+python3.11 "scripts/explain_eval.py" --metric AUC --continue_run True --model_type dpsgd
+python3.11 "scripts/explain_eval.py" --metric AUC --continue_run True --model_type dpsgd
+python3.11 "scripts/explain_eval.py" --metric AUC --continue_run True --model_type dpsgd
+python3.11 "scripts/explain_eval.py" --metric AUC --continue_run True --model_type dpsgd
+python3.11 "scripts/explain_eval.py" --metric AUC --continue_run True --model_type dpsgd
+python3.11 "scripts/explain_eval.py" --metric AUC --continue_run True --model_type dpsgd
+python3.11 "scripts/explain_eval.py" --metric AUC --continue_run True --model_type dpsgd
+python3.11 "scripts/explain_eval.py" --metric AUC --continue_run True --model_type dpsgd
+python3.11 "scripts/explain_eval.py" --metric AUC --continue_run True --model_type dpsgd
+python3.11 "scripts/explain_eval.py" --metric AUC --continue_run True --model_type dpsgd
+python3.11 "scripts/explain_eval.py" --metric AUC --continue_run True --model_type dpsgd
+python3.11 "scripts/explain_eval.py" --metric AUC --continue_run True --model_type dpsgd
+python3.11 "scripts/explain_eval.py" --metric AUC --continue_run True --model_type dpsgd
+python3.11 "scripts/explain_eval.py" --metric AUC --continue_run True --model_type dpsgd
+python3.11 "scripts/explain_eval.py" --metric AUC --continue_run True --model_type dpsgd
+python3.11 "scripts/explain_eval.py" --metric AUC --continue_run True --model_type dpsgd
+python3.11 "scripts/explain_eval.py" --metric AUC --continue_run True --model_type dpsgd
+python3.11 "scripts/explain_eval.py" --metric AUC --continue_run True --model_type dpsgd
+python3.11 "scripts/explain_eval.py" --metric AUC --continue_run True --model_type dpsgd
+python3.11 "scripts/explain_eval.py" --metric AUC --continue_run True --model_type dpsgd
+python3.11 "scripts/explain_eval.py" --metric AUC --continue_run True --model_type dpsgd
+python3.11 "scripts/explain_eval.py" --metric AUC --continue_run True --model_type dpsgd
+python3.11 "scripts/explain_eval.py" --metric AUC --continue_run True --model_type dpsgd
+python3.11 "scripts/explain_eval.py" --metric AUC --continue_run True --model_type dpsgd
+python3.11 "scripts/explain_eval.py" --metric AUC --continue_run True --model_type dpsgd
+python3.11 "scripts/explain_eval.py" --metric AUC --continue_run True --model_type dpsgd
+python3.11 "scripts/explain_eval.py" --metric AUC --continue_run True --model_type dpsgd
+python3.11 "scripts/explain_eval.py" --metric AUC --continue_run True --model_type dpsgd
+python3.11 "scripts/explain_eval.py" --metric AUC --continue_run True --model_type dpsgd
